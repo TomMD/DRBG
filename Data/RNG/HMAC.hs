662f6814dd626aed12becda2fb6129ed6c7fc800
thomas.dubuisson <thomas.dubuisson@gmail.com>
1999-12-31 16:12:40 -0800

    Add HMAC drbg
    Ignore-this: adb971941017eced4f149be3b563e0ad
    
    darcs-hash:20000101001240-cef97-8d9f93ac9962e9e470f16ccc75169d2249ec24ef.gz

---
+++ Data/RNG/HMAC.hs
@@ -0,0 +1,60 @@
+module Data.RNG.HMAC
+
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as L
+import Data.Serialize (encode)
+import Data.Crypto.Classes
+
+
+data State h = St
+	{ value		:: !B.ByteString
+	, key		:: !B.ByteString
+	, counter	:: !Integer
+	-- Start admin info
+	, securityStrength :: !Int
+	, predictionResistant :: !Bool
+	, hashAlg	:: h
+	} deriving (Eq, Ord)
+
+
+update :: (Hash h d) => State h -> L.ByteString -> State H
+update st input = st { value = newV , key = newk }
+  where
+  h  = hashAlg st
+  k  = key st
+  v  = value st
+  k' = hmac h k (L.concat [L.fromChunks [v], L.singleton 0, input])
+  v' = hmac h k' v
+  (newK, newV) =
+    if L.length input == 0
+      then (k',v')
+      else let k'' = hmac h k (L.concat [L.fromChunks [v'], L.singleton 1, input])
+           in (k'', hmac h k'' v')
+
+instantiate :: (Hash h d) => h -> Entropy -> Nonce -> PersonalizationString -> Stat h
+instantiate h ent nonce perStr =
+	let seedMaterial = B.concat [ent, nonce, perStr]
+	    k = B.replicate (bitLength h) 0
+	    v = B.replicate (bitLength h) 1
+	in update (St v k 1 (strength h) True h)
+
+reseed :: (Hash h d) => State h -> Entropy -> AdditionalInput -> State h
+reseed st ent ai = (update st (B.append ent ai)) { counter = 1 }
+
+generate :: (Hash h d) => State h -> BitLen -> AdditionalInput -> Maybe (RandomBits, State h)
+generate st req additionalInput =
+	if(counter st > reseed_interval)
+		then Nothing
+		else Just (wFinal, stFinal { counter = 1 + counter st})
+  where
+  c = counter st
+  st' = if B.length additionalInput == 0
+		then 0
+		else update st additionalInput
+  retBits = B.take reqBytes (B.concat . take m . map snd $ w)
+  reqBytes = r `div` 8
+  m = if req `rem` outlen == 0 then req `div` outlen else (r + outlen) `div` outlen
+  w = iterate (\((kI,vI),wOld) -> let vN = hmac h k v in ((kI, vN), L.append wOld vN)) ((key st', value st'), L.empty)
+  ((kFinal, vFinal), wFinal) = head $ drop m w
+  stFinal = update (st { key = kFinal, value = vFinal}) additionalInput
+  outlen = outputLength h
