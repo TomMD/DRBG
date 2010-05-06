7ab85ace0c444472f4842b1de71c1aa48c3f139b
thomas.dubuisson <thomas.dubuisson@gmail.com>
2010-05-06 12:49:40 -0700

    Cipher instance
    Ignore-this: 5060e05bc7c4f721b6b54e56eea816b0
    
    darcs-hash:20100506194940-cef97-d0af2074003852a6e51eccc2ba4adce310b3f923.gz

--- Data/Crypto/Classes.hs
+++ Data/Crypto/Classes.hs
@@ -13,6 +13,28 @@ class (Binary d, Serialize d, Pretty d) => Hash h d | h -> d where
   outputLength :: h -> BitLength
   hashFunction :: h -> ByteString -> d
 
+class (Binary ct, Serialize ct) => Cipher c ct k | k -> c, c -> ct where
+  blockSize       :: c -> BitLength
+  cipherFunction  :: k -> ByteString -> ct
+  decryptFunction :: k -> ct -> ByteString
+  buildKey        :: ByteString -> Maybe k
+  keyLength       :: k -> BitLength
+
+
+{- Example instances 
+instance Hash SHA256 DigestSHA256 where
+  outputLength _ = 256
+  hashFunction = sha256
+
+instance Cipher AES AESCT KeyAES where
+  blockSize _ = 128
+  cipherFunction = aesE
+  decipherFunction = aesD
+  buildKey = aesExpandKey
+  keyLength = aesKeyLength
+
+-}
+
 -- class (Binary d, Serialize d, Pretty d) => Digest d h | h -> d where
 --   hash :: L.ByteString -> d
   
