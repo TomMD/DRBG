2b3f72666920e8fa90aa8f299da4f1797c718868
thomas.dubuisson <thomas.dubuisson@gmail.com>
2010-08-17 15:20:30 -0700

    Play around with KAT, update cabal
    Ignore-this: 522afb009acf1424a9e5844b5ecc5d7c
    
    darcs-hash:20100817222030-cef97-d3e87021437f79b08d7cae53bd7c208f98faa7da.gz

--- Test/KAT.hs
+++ Test/KAT.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE EmptyDataDecls, MultiParamTypeClasses #-}
 import qualified Data.DRBG.Hash as H
 import qualified Data.DRBG.HMAC as M
-import Data.Digest.Pure.SHA
+import Data.CryptoHash.SHA256 as SHA
 import Data.ByteString as B
 import Data.Crypto.Classes
 import Data.Serialize as Ser
@@ -10,86 +10,75 @@ import Data.Binary as Bin
 import Data.Binary.Put as P
 import Text.PrettyPrint.HughesPJClass
 import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Data.ByteString.Lazy as LN
 import Data.Bits (shiftR, shiftL)
 import Data.Crypto.HMAC
+import Data.Crypto.Types
 import Data.Digest.Pure.MD5 as M
 import Data.Bits (xor)
+import Data.Tagged
 
-data SHA256
+newtype SHADigest = SHADigest B.ByteString
+	deriving (Eq, Ord, Show)
 
-instance Show SHA256 where
-	show _ = "SHA256"
+instance Hash SHA.Ctx SHADigest where
+  outputLength = Tagged 256
+  initialCtx = SHA.init
+  updateCtx = SHA.update
+  finalize ctx = SHADigest . SHA.finalize . SHA.update ctx
+  strength = Tagged 256
+  blockLength = Tagged 512
 
-sha256desc = undefined :: SHA256
+instance H.SeedLength SHADigest where
+	seedlen = Tagged 440
 
-instance Hash SHA256 Int Digest where
-  outputLength _ = 256
-  hashFunction _ = sha256
-  initialContext _ = 0
-  updateContext = undefined
-  finalize = undefined
-  strength _ = 256
-  blockLength _ = 512
-
-data MD5
-instance Show MD5 where
-  show _ = "MD5"
-
-instance Hash MD5 MD5Context MD5Digest where
-  outputLength _ = 128
-  hashFunction _ = M.md5
-  initialContext _ = md5InitialContext
-  updateContext = md5Update
-  finalize = md5Finalize
-  strength _ = 128
-  blockLength _ = 512
-
-instance H.SeedLength SHA256 where
-	seedlen _ = 440
-
-instance Serialize Digest where
+instance Serialize SHADigest where
 	get = undefined
-	put d = S.putByteString (B.concat $ L.toChunks (bytestringDigest d))
-instance Binary Digest where
+	put (SHADigest d) = S.putByteString d
+
+instance Binary SHADigest where
 	get = undefined
-	put = P.putLazyByteString . bytestringDigest
-
-instance Pretty Digest where
-	pPrint = undefined
-
-instance Pretty MD5Digest where
-	pPrint = undefined
+	put (SHADigest d) = P.putByteString d
 
 main = hmacMain >> hashMain >> hmacCipher
 
 hmacCipher = do
-	print (Ser.encode (hmac hsh k d) == res)
+	print (Ser.encode (hmac k d `asTypeOf` hsh) == res)
+	print (B.unpack calcVal)
+	print (B.unpack res)
   where
-  hash = hashFunction hsh
-  hsh = (undefined :: SHA256)
+  calcVal = Ser.encode (hmac k d `asTypeOf` hsh)
+  hash = hashFunc hsh
+  hsh = (undefined :: SHADigest)
   k = B.replicate 32 0x0b
   d = L.pack "Hi There"
   res = i2bs 256 0x198a607eb44bfbc69903a0f1cf2bbdc5ba0aa3f3d9ae3c1c7a3b1696a0b68cf7
+--  d = B.concat $ L.toChunks $ L.pack "good"
+--  k = L.pack "hello"
+--  res = i2bs 256 0x700524d54e5227a5a07bed2a67b34ba29925a9f8212b16668a100eac15efdf2e
   d' = Bin.encode (hash $ L.concat [ki, d])
   k' = Ser.encode (hash (L.fromChunks [k]))
   ki = L.fromChunks [B.map (`xor` 0x36) k']
   ko = L.fromChunks [B.map (`xor` 0x5c) k']
 
-
+-- Test the HMAC DRBG functionallity
 hmacMain = do
-    let st = M.instantiate hsh entropy nonce perstr
+    let st = M.instantiate entropy nonce perstr :: M.State SHADigest
 	Just (_,st') = M.generate st 256 additional
 	st'' = M.reseed st' entropyRS additional
         Just (r1,_) = M.generate st'' 256 additional
     print $ Prelude.map (==res) [r1]
+    print $ LN.unpack r1
+    print $ LN.unpack res
+    print $ L.length r1
+    print $ L.length res
   where
-  hsh = (undefined :: SHA256)
-  entropy = i2bs 256 0xd267c90786019cf0e82dfbde3864e2364e45ee80f81ad48f95a824eef03d3d8c
-  nonce = i2bs 128 0x171e633600ab8fe6d4cab13622315db1
   perstr = B.empty
   additional = B.empty
-  entropyRS  = i2bs 256 0xb1177e226b0d306cc914360b570e3ffe918dc1045aff9066c219c102f888c09b
-  res = L.fromChunks [i2bs 256 0x2f118de4d28fafb44bced1d26070a2d8dd1b477806ee6136db5dae26bf18e71c]
+  entropy = i2bs 256 0xebd11132d7837960500a436e467aba7dd28546faf6e74fa9950c56efb405505e
+  nonce = i2bs 128 0x9a5ebbb0fd780a00d52ee438e6f87084
+  entropyRS  = i2bs 256 0x785dd9360f2f52aa9153eb726536fcd470c75a9b6805d63b77c5f74113c8faff
+  res = L.fromChunks [i2bs 256 0x98ebbadbee0d67f7b8b70750b0da5e7d90572682b357bf580ed88c94529cce7f]
 {-
   entropy = i2bs 256 0xebd11132d7837960500a436e467aba7dd28546faf6e74fa9950c56efb405505e
   nonce = i2bs 128 0x9a5ebbb0fd780a00d52ee438e6f87084
@@ -100,13 +89,13 @@ hmacMain = do
 -}
 
 hashMain = do
-    let st  = H.instantiate hsh entropy nonce personalStr
+    let st  = H.instantiate entropy nonce personalStr :: H.State SHADigest
 	Just (_,st') = H.generate st 256 additional
 	st'' = H.reseed st' reseedIn additional
 	Just (rb1, _) = H.generate st'' 256 additional
     print [rb1 == res]
   where
-  hsh = (undefined :: SHA256) 
+  hsh = (undefined :: SHADigest) 
   entropy = i2bs 256 0xc8850054b417efb9325b5782b63b3be7a8a444949d742636d9a5303e04b933fa
   nonce   = i2bs 128 0x52a81de4b094c470abd675eb05695704
   reseedIn = i2bs 256 0xfab438f89d3839fe202e0da304d1479c34e4f574fc5cc0d8465146231a26b62c
