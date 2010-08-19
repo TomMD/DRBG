3d34f8ae48fa30eaeb5f55d3fa9a8c60da24c185
thomas.dubuisson <thomas.dubuisson@gmail.com>
2010-08-18 19:31:58 -0700

    Add KAT parsing/tests for HMAC DRBG
    Ignore-this: 1750a6f37e71d32f900ccdc4d4a81758
    
    darcs-hash:20100819023158-cef97-b111bee1d7c76a958c15fc950a164337a37969ee.gz

--- Test/KAT.hs
+++ Test/KAT.hs
@@ -2,7 +2,7 @@
 import qualified Data.DRBG.Hash as H
 import qualified Data.DRBG.HMAC as M
 import Data.CryptoHash.SHA256 as SHA
-import Data.ByteString as B
+import qualified Data.ByteString as B
 import Data.Crypto.Classes
 import Data.Serialize as Ser
 import Data.Serialize.Put as S
@@ -14,9 +14,13 @@ import qualified Data.ByteString.Lazy as LN
 import Data.Bits (shiftR, shiftL)
 import Data.Crypto.HMAC
 import Data.Crypto.Types
-import Data.Digest.Pure.MD5 as M
 import Data.Bits (xor)
 import Data.Tagged
+import Data.Maybe (maybeToList)
+import Data.List (deleteBy)
+import Test.Crypto
+import Test.ParseNistKATs
+import Text.Parsec.ByteString
 
 newtype SHADigest = SHADigest B.ByteString
 	deriving (Eq, Ord, Show)
@@ -42,6 +46,7 @@ instance Binary SHADigest where
 
 main = hmacMain >> hashMain >> hmacCipher
 
+-- Verify the HMAC implementation
 hmacCipher = do
 	print (Ser.encode (hmac k d `asTypeOf` hsh) == res)
   where
@@ -51,14 +56,75 @@ hmacCipher = do
   k = B.replicate 32 0x0b
   d = L.pack "Hi There"
   res = i2bs 256 0x198a607eb44bfbc69903a0f1cf2bbdc5ba0aa3f3d9ae3c1c7a3b1696a0b68cf7
---  d = B.concat $ L.toChunks $ L.pack "good"
---  k = L.pack "hello"
---  res = i2bs 256 0x700524d54e5227a5a07bed2a67b34ba29925a9f8212b16668a100eac15efdf2e
   d' = Bin.encode (hash $ L.concat [ki, d])
   k' = Ser.encode (hash (L.fromChunks [k]))
   ki = L.fromChunks [B.map (`xor` 0x36) k']
   ko = L.fromChunks [B.map (`xor` 0x5c) k']
 
+
+nistTests_HMAC :: IO [Test]
+nistTests_HMAC = do
+	(Right cats) <- parseFromFile (many parseCategory) "HMAC_DRBG.txt"
+	return (concat $ concatMap (maybeToList . categoryToTest_HMAC) cats)
+
+-- Currently run SHA-256 tests only
+categoryToTest_HMAC :: TestCategory -> Maybe [Test]
+categoryToTest_HMAC (props, ts) =
+	if "SHA-256" `notElem` map fst props
+		then Nothing
+		else let s = unlines $ map showProp props 
+			 h = hashFunc (undefined :: SHADigest)
+			 tests = concatMap (map katToTest . maybeToList . buildKAT) ts
+		     in Just tests
+  where
+  deleteF k lst = deleteBy (const $ (==) k . fst) undefined lst
+  isPR = Just True == fmap read (lookup "PredictionResistance" props)
+  showProp (p,"") = '[' : p ++ "]"
+  showProp (p,v)  = '[' : p ++ " = " ++ v ++ "]"
+  testName = fst (head props) ++ (if isPR then "_PR" else "")
+--   buildKat :: [Record] -> Maybe (KAT (String, String, String, String, String, String, String) B.ByteString)
+  buildKAT t
+	| fmap read (lookup "PredictionResistance" props) == Just True = do
+	cnt    <- lookup "COUNT" t
+	let name = testName ++ cnt
+	eIn    <- lookup "EntropyInput" t
+	n      <- lookup "Nonce" t
+	per    <- lookup "PersonalizationString" t
+	aIn1   <- lookup "AdditionalInput" t
+	eInPR1 <- lookup "EntropyInputPR" t
+	let t' = deleteF "EntropyInputPR" (deleteF "AdditionalInput" t)
+	aIn2   <- lookup "AdditionalInput" t'
+	eInPR2 <- lookup "EntropyInputPR" t'
+	ret    <- lookup "ReturnedBits" t'
+	let f (eIn, n, per, aIn1, eInPR1, aIn2, eInPR2) =
+		let hx = hexStringToBS
+		    st0 = M.instantiate (hx eIn) (hx n) (hx per) :: M.State SHADigest
+		    st1 = M.reseed st0 (hx eInPR1) (hx aIn1)
+		    Just (_,st2) = M.generate st1 256 B.empty
+		    st3 = M.reseed st2 (hx eInPR2) (hx aIn2)
+		    Just (r1,_) = M.generate st3 256 B.empty
+		in r1
+	return (K (eIn, n, per, aIn1, eInPR1, aIn2, eInPR2) f (L.fromChunks [hexStringToBS ret]) name)
+	| otherwise = do
+	cnt <- lookup "COUNT" t
+	let name = testName ++ cnt
+	eIn   <- lookup "EntropyInput" t
+	n     <- lookup "Nonce" t
+	per   <- lookup "PersonalizationString" t
+	aIn1  <- lookup "AdditionalInput" t
+	eInRS <- lookup "EntropyInputReseed" t
+	aInRS <- lookup "AdditionalInputReseed" t
+	let t' = deleteF "AdditionalInput" t
+	aIn2  <- lookup "AdditionalInput" t'
+	ret   <- lookup "ReturnedBits" t
+	let f (eIn, n, per, aIn1, eInRS, aInRS, aIn2) =
+		let hx = hexStringToBS
+		    st0 = M.instantiate (hx eIn) (hx n) (hx per) :: M.State SHADigest
+		    Just (_,st1) = M.generate st0 256 (hx aIn1)
+		    st2 = M.reseed st1 (hx eInRS) (hx aInRS)
+		    Just (r1, _) = M.generate st2 256 (hx aIn2)
+		in r1
+	return (K (eIn, n, per, aIn1, eInRS, aInRS, aIn2) f (L.fromChunks [hexStringToBS ret]) name)
 -- Test the HMAC DRBG functionallity
 hmacMain = do
     let st = M.instantiate entropy nonce perstr :: M.State SHADigest
@@ -82,6 +148,7 @@ hmacMain = do
   res = L.fromChunks [i2bs 256 0x98ebbadbee0d67f7b8b70750b0da5e7d90572682b357bf580ed88c94529cce7f]
 -}
 
+-- Verify the Hash-DRBG operation
 hashMain = do
     let st  = H.instantiate entropy nonce personalStr :: H.State SHADigest
 	Just (_,st') = H.generate st 256 additional
